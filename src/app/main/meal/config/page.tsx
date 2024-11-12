a080c95aaaf650899bb5d46ef142bb64bfb2efff
aksel26 <kevinxkim2023@gmail.com>
2024-11-12 18:10:56 +0900

    식대 설정 페이지

--- src/app/main/meal/config/page.tsx
+++ src/app/main/meal/config/page.tsx
@@ -1,21 +1,10 @@
 "use client";
-import { ActionIcon, Button, Divider, Group, Input, Menu, NumberFormatter, NumberInput, Pagination, Stack, Table, Text } from "@mantine/core";
+import { ActionIcon, Box, Button, Divider, Group, Input, Menu, NumberFormatter, NumberInput, Pagination, Paper, Stack, Table, Text } from "@mantine/core";
 import React, { useState } from "react";
 import More from "/public/icons/dots.svg";
-const elements = [
-  { position: 1, mass: Math.random() * 100000, symbol: "본부장", name: "김현근", etc: "11월 23일 퇴사예정" },
-  { position: 2, mass: Math.random() * 100000, symbol: "본부장", name: "박민수", etc: "11월 1일부 육아휴직" },
-  { position: 3, mass: Math.random() * 100000, symbol: "팀장", name: "박민숙" },
-  { position: 7, mass: Math.random() * 100000, symbol: "N", name: "Nitrogen" },
-  { position: 39, mass: Math.random() * 100000, symbol: "Y", name: "Yttrium" },
-  { position: 56, mass: Math.random() * 100000, symbol: "Ba", name: "Barium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-];
+const elements = Array.from({ length: 41 }, (_, index) => {
+  return { position: index + 1, grade: "본부장", balance: 1500, expense: 75300, amount: 890000, name: "김현근2", etc: "12일 퇴사 예정" };
+});
 function page() {
   const [total, setTotal] = useState(0);
   const [result, setResult] = useState(0);
@@ -25,11 +14,12 @@ function page() {
     console.log("🚀 ~ defaultPrice ~ e:", e);
   };
   const rows = elements.map((element) => (
-    <Table.Tr key={element.name}>
+    <Table.Tr key={element.position}>
       <Table.Td>{element.position}</Table.Td>
+      <Table.Td>{element.grade}</Table.Td>
       <Table.Td>{element.name}</Table.Td>
       <Table.Td>
-        <NumberFormatter thousandSeparator value={430000} suffix=" 원" />
+        <NumberFormatter thousandSeparator value={element.amount} suffix=" 원" />
       </Table.Td>
       <Table.Td>{element.etc}</Table.Td>
       <Table.Td>
@@ -49,55 +39,55 @@ function page() {
     </Table.Tr>
   ));
   return (
-    <>
+    <Box pb={50}>
       <Text fw={900} size="xl" mb={"xl"}>
         식대 설정
       </Text>
       <Stack mb={"xl"} gap={"sm"}>
-        <Text size="md" fw={700}>
-          기본 식대 금액 설정
-        </Text>
-        <Group align="flex-end">
-          <NumberInput label="기본 제공 식대" placeholder="금액을 입력해 주세요." thousandSeparator="," hideControls suffix=" 원" onChange={defaultPrice} />
+        <Paper p="lg" withBorder radius={"lg"} px={"xl"} w={"max-content"}>
+          <Text size="md" fw={700} mb={"md"}>
+            기본 식대 금액 설정
+          </Text>
+          <Group align="flex-end" gap={"xl"}>
+            <NumberInput label="기본 제공 식대" placeholder="금액을 입력해 주세요." thousandSeparator="," hideControls suffix=" 원" onChange={defaultPrice} />
 
-          <NumberInput label="업무일" thousandSeparator="," hideControls suffix=" 일" value={23} />
+            <NumberInput label="업무일" thousandSeparator="," hideControls suffix=" 일" value={23} />
 
-          <NumberInput
-            label={
-              <Group>
-                인원별 총 금액
-                <Text c={"gray.7"} size="xs">
-                  기본제공 식대 x 업무일
-                </Text>
-              </Group>
-            }
-            variant="unstyled"
-            thousandSeparator=","
-            hideControls
-            suffix=" 원"
-            value={result}
-          />
-          <Button radius={"md"}>저장</Button>
-        </Group>
+            <NumberInput
+              label={
+                <Group>
+                  인원별 총 금액
+                  <Text c={"gray.7"} size="xs">
+                    기본제공 식대 x 업무일
+                  </Text>
+                </Group>
+              }
+              variant="unstyled"
+              thousandSeparator=","
+              hideControls
+              suffix=" 원"
+              value={result}
+            />
+            <Button radius={"md"}>저장</Button>
+          </Group>
+        </Paper>
       </Stack>
       <Divider my="md" />
 
-      <Table striped>
+      <Table striped stickyHeader stickyHeaderOffset={50} highlightOnHover>
         <Table.Thead>
           <Table.Tr>
             <Table.Th>No.</Table.Th>
-            <Table.Th>사용자</Table.Th>
-            <Table.Th>금액</Table.Th>
+            <Table.Th>직급</Table.Th>
+            <Table.Th>성명</Table.Th>
+            <Table.Th>총 금액</Table.Th>
             <Table.Th>비고</Table.Th>
-            <Table.Th></Table.Th>
+            <Table.Th />
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>{rows}</Table.Tbody>
       </Table>
-      <Group justify="center">
-        <Pagination total={10} radius="md" />
-      </Group>
-    </>
+    </Box>
   );
 }
 
