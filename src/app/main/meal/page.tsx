ab80a068e0f6b8367081fedf3057c1869886e76b
aksel26 <kevinxkim2023@gmail.com>
2024-11-12 18:11:07 +0900

    식대 조회 페이지 재구성

--- src/app/main/meal/page.tsx
+++ src/app/main/meal/page.tsx
@@ -1,71 +1,82 @@
 "use client";
 import { ActionIcon, Box, Button, Center, Flex, Group, Input, Menu, NumberFormatter, Pagination, Stack, Table, Text } from "@mantine/core";
 import React, { useState } from "react";
-import Adjust from "/public/icons/adjustments-alt.svg";
-const elements = [
-  { position: 1, mass: Math.random() * 100000, symbol: "정읍식당", name: "김현근 본부장" },
-  { position: 2, mass: Math.random() * 100000, symbol: "포케", name: "박민수 본부장" },
-  { position: 7, mass: Math.random() * 100000, symbol: "N", name: "Nitrogen" },
-  { position: 39, mass: Math.random() * 100000, symbol: "Y", name: "Yttrium" },
-  { position: 56, mass: Math.random() * 100000, symbol: "Ba", name: "Barium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-  { position: 58, mass: Math.random() * 100000, symbol: "Ce", name: "Cerium" },
-];
+import IconAdjust from "/public/icons/adjustments-alt.svg";
+import IconDownload from "/public/icons/download.svg";
+import { DatePickerInput } from "@mantine/dates";
+const elements = Array.from({ length: 40 }, (_, index) => {
+  return {
+    position: index + 1,
+    grade: "본부장",
+    place: "땀땀땀",
+    balance: 1500,
+    expense: 75300,
+    amount: 890000,
+    name: "김현근2",
+    etc: "12일 퇴사 예정",
+    targetDay: "2024-11-23",
+  };
+});
 function page() {
+  const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
+
   const rows = elements.map((element) => (
-    <Table.Tr key={element.name}>
+    <Table.Tr key={element.position}>
       <Table.Td>{element.position}</Table.Td>
+      <Table.Td>{element.grade}</Table.Td>
       <Table.Td>{element.name}</Table.Td>
-      <Table.Td>{element.symbol}</Table.Td>
-      <Table.Td>
-        <NumberFormatter thousandSeparator value={430000} suffix=" 원" />
-      </Table.Td>
+      <Table.Td>{element.place}</Table.Td>
+
       <Table.Td>
         <NumberFormatter thousandSeparator value={12000} suffix=" 원" />
       </Table.Td>
-      <Table.Td>2024-11-20</Table.Td>
+      <Table.Td>{element.targetDay}</Table.Td>
     </Table.Tr>
   ));
   return (
-    <Flex direction={"column"} justify={"space-between"} h={"100%"} pb={"md"}>
+    <Flex direction={"column"} justify={"space-between"} pb={50}>
       <Box>
         <Text fw={900} size="xl" mb={"xl"}>
           식대 내역 조회
         </Text>
         <Group justify="space-between" mb={"md"}>
-          <Group gap={"xs"}>
-            <Input placeholder="검색할 내용을 입력해 주세요." radius="md" />
+          <Group gap={"xs"} align="end">
+            <DatePickerInput type="range" label="작성일" placeholder="작성일 선택" value={value} onChange={setValue} />
+            <Input.Wrapper label="성명">
+              <Input placeholder="검색 대상의 성명을 입력해 주세요." radius="md" />
+            </Input.Wrapper>
+
             <Button size="sm" radius={"md"}>
               검색
             </Button>
           </Group>
-          <Menu shadow="md">
-            <Menu.Target>
-              <ActionIcon variant="light" size={"lg"}>
-                <Adjust width="20" height="20" strokeWidth="1.5" />
-              </ActionIcon>
-            </Menu.Target>
+          <Group>
+            <Button variant="light" size="sm" radius={"md"} rightSection={<IconDownload width="15" height="15" />}>
+              내려받기
+            </Button>
+            <Menu shadow="md">
+              <Menu.Target>
+                <ActionIcon variant="light" size={"lg"}>
+                  <IconAdjust width="20" height="20" strokeWidth="1.5" />
+                </ActionIcon>
+              </Menu.Target>
 
-            <Menu.Dropdown>
-              <Menu.Label>정렬</Menu.Label>
-              <Menu.Item>이름순</Menu.Item>
-              <Menu.Item>금액순</Menu.Item>
-              <Menu.Item>등록순</Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
+              <Menu.Dropdown>
+                <Menu.Label>정렬</Menu.Label>
+                <Menu.Item>등록순</Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </Group>
         </Group>
-        <Table striped>
+
+        <Table striped stickyHeader stickyHeaderOffset={50} highlightOnHover>
           <Table.Thead>
             <Table.Tr>
               <Table.Th>No.</Table.Th>
-              <Table.Th>사용자</Table.Th>
+              <Table.Th>직급</Table.Th>
+              <Table.Th>성명</Table.Th>
               <Table.Th>사용처</Table.Th>
               <Table.Th>사용 금액</Table.Th>
-              <Table.Th>잔액</Table.Th>
               <Table.Th>작성일</Table.Th>
             </Table.Tr>
           </Table.Thead>
@@ -73,7 +84,7 @@ function page() {
         </Table>
       </Box>
 
-      <Group justify="center">
+      <Group justify="center" my={30}>
         <Pagination total={10} radius="md" />
       </Group>
     </Flex>
