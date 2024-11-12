dca754762ab792da013639585ff506bc025772f7
aksel26 <kevinxkim2023@gmail.com>
2024-11-12 14:36:35 +0900

    식대 통계

---
+++ src/app/main/meal/stats/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import {
+  ActionIcon,
+  Box,
+  Button,
+  Center,
+  Flex,
+  Grid,
+  Group,
+  Input,
+  Menu,
+  NumberFormatter,
+  Pagination,
+  Paper,
+  Skeleton,
+  Stack,
+  Table,
+  Text,
+} from "@mantine/core";
+import React, { useState } from "react";
+import Adjust from "/public/icons/adjustments-alt.svg";
+
+function page() {
+  return (
+    <>
+      <Text fw={900} size="xl" mb={"xl"}>
+        식대 통계
+      </Text>
+      <Grid>
+        <Grid.Col span={2}>
+          <Paper shadow="xl" radius={"lg"} p={"md"}>
+            <Text fw={700} size="md" mb={"xs"}>
+              기본 식대 금액
+            </Text>
+            <NumberFormatter value={230000} thousandSeparator suffix="원" />
+          </Paper>
+        </Grid.Col>
+        <Grid.Col span={3}>
+          <Paper shadow="xl" radius={"lg"} p={"md"}>
+            <Text fw={700} size="md" mb={"xs"}>
+              이전 달 평균 초과금액{" "}
+            </Text>
+            <NumberFormatter value={51000} thousandSeparator suffix="원" />
+          </Paper>
+        </Grid.Col>
+        <Grid.Col span={3}>
+          <Paper shadow="xl" radius={"lg"} p={"md"}>
+            <Text fw={700} size="md" mb={"xs"}>
+              이전 달 초과금액 미정산자{" "}
+            </Text>
+            <Stack>
+              <Skeleton h={"sm"} />
+              <Skeleton h={"sm"} />
+              <Skeleton h={"sm"} />
+              <Skeleton h={"sm"} />
+              <Skeleton h={"sm"} />
+              <Skeleton h={"sm"} />
+              <Skeleton h={"sm"} />
+              <Skeleton h={"sm"} />
+            </Stack>
+            {/* <NumberFormatter value={51000} thousandSeparator suffix="원" /> */}
+          </Paper>
+        </Grid.Col>
+      </Grid>
+    </>
+  );
+}
+
+export default page;
