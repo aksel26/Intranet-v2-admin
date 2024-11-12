195bb71f4617b6852f12aabea34c01a62a5f523f
aksel26 <kevinxkim2023@gmail.com>
2024-11-12 14:32:25 +0900

    add svgr/webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    // SVGR 설정 추가
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true, // true로 설정하면 width와 height를 props로 전달 가능
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
