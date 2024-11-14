af37ba2660990668354fc9f84b01c76f3bae4b63
aksel26 <kevinxkim2023@gmail.com>
2024-11-14 18:45:46 +0900

    직원조회 화면 구성

--- src/app/main/staff/page.tsx
+++ src/app/main/staff/page.tsx
@@ -1,11 +1,16 @@
 "use client";
-import { ActionIcon, Box, Button, Center, Flex, Group, Input, Menu, NumberFormatter, Pagination, Popover, Select, Stack, Table, Text } from "@mantine/core";
-import React, { useState } from "react";
-import IconAdjust from "/public/icons/adjustments-alt.svg";
-import IconUserExclamation from "/public/icons/exclamation-circle.svg";
-import IconDownload from "/public/icons/download.svg";
+import { GRADE_NAME_LABEL, GRADE_NAME_LIST, JOIN_DATE_LABEL, STAFF_NAME_LABEL } from "@/app/enums/staffInfo";
+import { STAFF_TABLE_HEADER } from "@/app/enums/tableHeader";
+import { ActionIcon, Alert, Box, Button, Divider, Flex, Group, Input, Menu, Modal, Pagination, Popover, Select, Stack, Table, Text } from "@mantine/core";
 import { DatePickerInput } from "@mantine/dates";
+import { useState } from "react";
+import IconAdjust from "/public/icons/adjustments-alt.svg";
 import More from "/public/icons/dots.svg";
+import IconDownload from "/public/icons/download.svg";
+import IconInfo from "/public/icons/info-circle.svg";
+import IconUserExclamation from "/public/icons/exclamation-circle.svg";
+import { useDisclosure } from "@mantine/hooks";
+import JoinModal from "@/app/components/staff/JoinModal";
 const elements = Array.from({ length: 40 }, (_, index) => {
   return {
     position: index + 1,
@@ -20,10 +25,13 @@ const elements = Array.from({ length: 40 }, (_, index) => {
     hqName: "HR솔루션 본부",
     teamName: "HR Tech팀",
     userEmail: "asdf@asdf.com",
+    userId: "hkkim",
   };
 });
 function page() {
   const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
+  const [opened, { open, close }] = useDisclosure(false);
+  const [deleteModal, { open: openDeleteModal, close: closeDeleteModal }] = useDisclosure(false);
 
   const rows = elements.map((element) => (
     <Table.Tr key={element.position}>
@@ -31,6 +39,7 @@ function page() {
       <Table.Td>{`${element.hqName} ${element.teamName}`}</Table.Td>
       <Table.Td>{element.gradeName}</Table.Td>
       <Table.Td>{element.name}</Table.Td>
+      <Table.Td>{element.userId}</Table.Td>
       <Table.Td>{element.mobile}</Table.Td>
       <Table.Td>{element.address}</Table.Td>
       <Table.Td>{element.userEmail}</Table.Td>
@@ -54,7 +63,7 @@ function page() {
         }
       </Table.Td>
       <Table.Td>
-        <Menu shadow="md">
+        <Menu shadow="md" position="bottom-end">
           <Menu.Target>
             <ActionIcon variant="light" size={"sm"}>
               <More width="15" height="15" strokeWidth="1.5" />
@@ -63,7 +72,9 @@ function page() {
 
           <Menu.Dropdown>
             <Menu.Item>정보 수정</Menu.Item>
-            <Menu.Item color="red">삭제</Menu.Item>
+            <Menu.Item onClick={openDeleteModal} color="red">
+              삭제
+            </Menu.Item>
           </Menu.Dropdown>
         </Menu>
       </Table.Td>
@@ -77,15 +88,10 @@ function page() {
         </Text>
         <Group justify="space-between" mb={"md"}>
           <Group gap={"xs"} align="end">
-            <Select
-              label="직급"
-              //   placeholder="Pick value"
-              data={["본부장", "팀장", "책임", "선임", "위원"]}
-              defaultValue="위원"
-              clearable
-            />
-            <DatePickerInput type="range" label="입사일" placeholder="입사일 선택" value={value} onChange={setValue} />
-            <Input.Wrapper label="성명">
+            <Select label={GRADE_NAME_LABEL} data={GRADE_NAME_LIST} defaultValue="위원" clearable />
+            <DatePickerInput type="range" label={JOIN_DATE_LABEL} placeholder="입사일 선택" value={value} onChange={setValue} />
+            <Select label={"성별"} data={["남", "여"]} placeholder="성별" />
+            <Input.Wrapper label={STAFF_NAME_LABEL}>
               <Input placeholder="검색 대상의 성영을 입력해 주세요." radius="md" />
             </Input.Wrapper>
 
@@ -94,13 +100,13 @@ function page() {
             </Button>
           </Group>
           <Group>
-            <Button variant="light" size="sm" radius={"md"}>
-              등록
+            <Button variant="light" size="sm" radius={"md"} onClick={open}>
+              직원 등록
             </Button>
             <Button variant="light" size="sm" radius={"md"} rightSection={<IconDownload width="15" height="15" />}>
               내려받기
             </Button>
-            <Menu shadow="md">
+            <Menu shadow="md" position="bottom-end">
               <Menu.Target>
                 <ActionIcon variant="light" size={"lg"}>
                   <IconAdjust width="20" height="20" strokeWidth="1.5" />
@@ -109,7 +115,10 @@ function page() {
 
               <Menu.Dropdown>
                 <Menu.Label>정렬</Menu.Label>
-                <Menu.Item>등록순</Menu.Item>
+                <Menu.Item>직급</Menu.Item>
+                <Menu.Item>생년월일</Menu.Item>
+                <Menu.Item>입사일</Menu.Item>
+                <Menu.Item>소속</Menu.Item>
               </Menu.Dropdown>
             </Menu>
           </Group>
@@ -118,18 +127,9 @@ function page() {
         <Table striped stickyHeader stickyHeaderOffset={50} highlightOnHover>
           <Table.Thead>
             <Table.Tr>
-              <Table.Th>No.</Table.Th>
-              <Table.Th>소속</Table.Th>
-              <Table.Th>직급</Table.Th>
-              <Table.Th>성명</Table.Th>
-              <Table.Th>연락처</Table.Th>
-              <Table.Th>주소</Table.Th>
-              <Table.Th>이메일</Table.Th>
-              <Table.Th>성별</Table.Th>
-              <Table.Th>생년월일</Table.Th>
-              <Table.Th>입사일</Table.Th>
-              <Table.Th>특이사항</Table.Th>
-              <Table.Th />
+              {STAFF_TABLE_HEADER.map((item: string) => (
+                <Table.Th>{item}</Table.Th>
+              ))}
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>{rows}</Table.Tbody>
@@ -139,6 +139,41 @@ function page() {
       <Group justify="center" my={30}>
         <Pagination total={10} radius="md" />
       </Group>
+      <Modal opened={opened} onClose={close} title="직원 등록" centered>
+        <JoinModal />
+      </Modal>
+      <Modal opened={deleteModal} onClose={closeDeleteModal} centered title="직원 삭제">
+        <Stack>
+          <Alert variant="outline" color="red" radius="md" title="해당 직원을 삭제하시겠습니까?" icon={<IconInfo />}>
+            삭제 후 되돌릴 수 없습니다.
+            <Group mt={"sm"} gap={"xs"}>
+              <Text size="xs" c={"gray.7"}>
+                김현근2
+              </Text>
+              <Divider orientation="vertical" size={"sm"} />
+              <Text size="xs" c={"gray.7"}>
+                hkkim
+              </Text>
+              <Divider orientation="vertical" size={"sm"} />
+              <Text size="xs" c={"gray.7"}>
+                010-3232-2322
+              </Text>
+              <Divider orientation="vertical" size={"sm"} />
+              <Text size="xs" c={"gray.7"}>
+                asdf@asdf.com{" "}
+              </Text>
+            </Group>
+          </Alert>
+          <Group wrap="nowrap">
+            <Button variant="light" color="red" fullWidth>
+              삭제하기
+            </Button>
+            <Button variant="light" color="gray" fullWidth onClick={closeDeleteModal}>
+              닫기
+            </Button>
+          </Group>
+        </Stack>
+      </Modal>
     </Flex>
   );
 }
