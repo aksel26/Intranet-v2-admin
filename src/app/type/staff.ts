84fa91ae1c8ea149a170d66326f7be08e45a9e13
aksel26 <kevinxkim2023@gmail.com>
2024-11-18 18:39:44 +0900

    types

---
+++ src/app/type/staff.ts
@@ -0,0 +1,17 @@
+export interface TStaffs {
+  userIdx: number;
+  id: string;
+  userName: string;
+  userGender: string;
+  userCell: string;
+  userAddress: string | null;
+  userEmail: string;
+  userBirth: string;
+  joinDate: string;
+  hqName: string | null;
+  teamName: string;
+  gradeName: string;
+  adminRole: string;
+  comment: string | null;
+  userAvail: null;
+}
