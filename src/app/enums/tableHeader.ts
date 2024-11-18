28b8f6f171ef9224ed9c45480664898ff45546ba
aksel26 <kevinxkim2023@gmail.com>
2024-11-18 18:38:26 +0900

    table header enums

--- src/app/enums/tableHeader.ts
+++ src/app/enums/tableHeader.ts
@@ -1,1 +1,3 @@
-export const STAFF_TABLE_HEADER = ["No", "소속", "직급", "성명", "ID", "연락처", "주소", "이메일", "성별", "생년월일", "입사일", "특이사항", ""];
+export const STAFF_TABLE_HEADER = ["No", "소속", "팀명", "직급", "성명", "ID", "연락처", "주소", "이메일", "성별", "생년월일", "입사일", "특이사항", ""];
+
+export const MEAL_SETTLEMENT_HEADER = ["", "No.", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산여부", "비고"];
