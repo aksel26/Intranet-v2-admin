629ee83ead33c91aa965c1e6313a3481018e6634
aksel26 <kevinxkim2023@gmail.com>
2024-11-19 19:12:05 +0900

    복포 금액 일괄설정, 개별설정 API 추가

--- src/app/api/post/postApi.ts
+++ src/app/api/post/postApi.ts
@@ -21,26 +21,6 @@ postApi.interceptors.request.use(
   }
 );
 
-// export const getMeals = () => jsonPlaceholderApi.get('/posts');
-
-// export const logout = () => postApi.post(`/logout`);
-// export const submitForm = (values: any) => postApi.post(`/users/qna`, values);
-
-// export const submitMeal = (values: any) => postApi.post(`/users/meals`, values);
-// export const submitWelfare = (values: any) => postApi.post(`/users/welfares`, values);
-// export const submitActivity = (values: any) => postApi.post(`/users/activities`, values);
-// export const submitUpdateWelfare = (values: any) => postApi.put(`/users/welfares/${values.queryParams}`, values.body);
-// export const submitUpdateActivity = (values: any) => postApi.put(`/users/activities/${values.queryParams}`, values.body);
-
-// export const deleteMeal = (values: any) => postApi.delete(`/users/meals/${values}`);
-// export const deleteWelfare = (values: any) => postApi.delete(`/users/welfares/${values}`);
-// export const deleteActivity = (values: any) => postApi.delete(`/users/activities/${values}`);
-// export const deleteQna = (values: any) => postApi.delete(`/users/qna`, { data: values });
-// export const getWelfares = ({ year, month }: TWelfaresParams) => getApi.get(`/users/welfares`, { params: { year, month } });
-// export const getActivities = ({ year, month }: TWelfaresParams) => getApi.get(`/users/activities`, { params: { year, month } });
-// export const getMe = () => getApi.get(`/users/me`);
-// export const getUsers = () => getApi.get(`/users/ids`);
-
 // 어드민 식대 설정 비고 수정 API
 export const updateMealBudgetNote = (values: any) =>
   postApi.patch(`/admin/meals/budget/${values.queryParams}`, values.body, {
@@ -58,7 +38,9 @@ export const settlement = (values: any) => postApi.patch(`/admin/meals/balances`
 // 어드민 식대 정산 완료 처리 API
 export const settlementCancel = (values: any) => postApi.patch(`/admin/meals/balances/cancel`, values);
 
+// 어드민 복포 설정 일괄 등록 및 수정 API
+export const updateWelfarePointBudget = (values: any) => postApi.post(`/admin/welfares/budget`, values);
 
-
-
-
+// 어드민 복포 설정 금액 개별 수정
+export const updateWelfarePointByPerson = (values: any) =>
+  postApi.patch(`/admin/welfares/budget/${values.params}`, values.body, { params: { welfareStatsIdx: values.params } });
