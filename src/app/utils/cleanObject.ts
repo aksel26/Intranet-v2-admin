cbefaff714f648c79d376fce7775cd2b0137ef75
aksel26 <kevinxkim2023@gmail.com>
2024-11-29 18:43:56 +0900

    cleanObject Date 처리

--- src/app/utils/cleanObject.ts
+++ src/app/utils/cleanObject.ts
@@ -1,10 +1,19 @@
+import dayjs from "dayjs";
+
 export const cleanObject = (obj: any, key: string) => {
   // Create a copy of the object to avoid mutating the original
   const cleanedObj = { ...obj };
 
+  if (cleanedObj.sDate === "Invalid Date") {
+    cleanedObj.sDate = dayjs().startOf("month").format("YYYY-MM-DD");
+  }
+  if (cleanedObj.eDate === "Invalid Date") {
+    cleanedObj.eDate = dayjs().endOf("month").format("YYYY-MM-DD");
+  }
+
   // Remove keys with empty string values
   Object.keys(cleanedObj).forEach((key) => {
-    if (cleanedObj[key] === "" || cleanedObj[key] === null || cleanedObj[key] === "Invalid Date") {
+    if (cleanedObj[key] === "" || cleanedObj[key] === null) {
       delete cleanedObj[key];
     }
   });
