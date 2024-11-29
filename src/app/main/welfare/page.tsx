d0717d392b592cc6027c47d88b6001e839cbac83
aksel26 <kevinxkim2023@gmail.com>
2024-11-29 18:43:15 +0900

    복지포인트 조회

--- src/app/main/welfare/page.tsx
+++ src/app/main/welfare/page.tsx
@@ -1,111 +1,180 @@
 "use client";
-import { HEIGHT } from "@/app/enums/design";
-import {
-  ActionIcon,
-  Alert,
-  Box,
-  Button,
-  Checkbox,
-  Flex,
-  Group,
-  Input,
-  Menu,
-  Modal,
-  NumberFormatter,
-  Pagination,
-  ScrollArea,
-  Stack,
-  Table,
-  Title,
-} from "@mantine/core";
+import * as api from "@/app/api/get/getApi";
+import * as postApi from "@/app/api/post/postApi";
+import PageList from "@/app/components/Global/PageList";
+import { ActionIcon, Alert, Button, Checkbox, Flex, Group, Input, Menu, Modal, NumberFormatter, ScrollArea, Select, Stack, Table, Title } from "@mantine/core";
 import { DatePickerInput } from "@mantine/dates";
 import { useDisclosure } from "@mantine/hooks";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import IconAdjust from "/public/icons/adjustments-alt.svg";
 import IconCircleChecked from "/public/icons/circle-dashed-check.svg";
 import IconDownload from "/public/icons/download.svg";
 import IconInfo from "/public/icons/info-circle.svg";
-import PageList from "@/app/components/Global/PageList";
 dayjs.locale("ko");
-const elements = Array.from({ length: 40 }, (_, index) => {
-  return {
-    position: index + 1,
-    grade: "본부장",
-    place: "맘맘테이블",
-    balance: 5400,
-    expense: 2323000,
-    amount: 434000,
-    name: "정진옥",
-    etc: "12일 퇴사 예정",
-    targetDay: "2024-11-23",
-    payee: "박민숙 또는 본인",
-  };
-});
+
+import { GRADE_NAME_LABEL } from "@/app/enums/staffInfo";
+import { TWelfares } from "@/app/type/welfare";
+import { cleanObject } from "@/app/utils/cleanObject";
+import notification from "@/app/utils/notification";
+import { useForm } from "@mantine/form";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+interface FormValues {
+  userName?: string;
+  gradeIdx?: string | null;
+  confirmYN?: number | null;
+}
+
 function page() {
-  const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
+  const [value, setValue] = useState<[Date | null, Date | null]>([dayjs().startOf("month").toDate(), dayjs().endOf("month").toDate()]);
   const [selectedRows, setSelectedRows] = useState<number[]>([]);
   const [check, { open: openCheck, close: closeCheck }] = useDisclosure(false);
-  const rows = elements.map((element) => (
-    <Table.Tr key={element.position} bg={selectedRows.includes(element.position) ? "var(--mantine-color-blue-light)" : undefined}>
+  const [searchParam, setSearchParam] = useState({
+    sDate: dayjs().startOf("month").format("YYYY-MM-DD"),
+    eDate: dayjs().endOf("month").format("YYYY-MM-DD"),
+    pageNo: 1,
+    userName: "",
+  });
+  const queryClient = useQueryClient();
+  const { data, isLoading, isError } = useQuery({ queryKey: ["welfares", searchParam], queryFn: () => api.getWelfares(searchParam) });
+  const {
+    data: gradeIds,
+    isLoading: gradeIds_isLoading,
+    isError: gradeIds_isError,
+  } = useQuery({
+    queryKey: ["gradeIds"],
+    queryFn: () => {
+      return api.getGradeIds();
+    },
+  });
+  const { mutate } = useMutation({
+    mutationFn: (values: any) => postApi.confirmWelfare(values),
+  });
+
+  const [gradeIdData, setGradeIdData] = useState();
+  useEffect(() => {
+    gradeIds &&
+      setGradeIdData(gradeIds?.data.data.map((item: { gradeIdx: number; gradeName: string }) => ({ value: item.gradeIdx.toString(), label: item.gradeName })));
+  }, [gradeIds]);
+  const confirmWelfare = () => {
+    mutate(
+      { welfareIdxList: selectedRows, confirmYN: "Y" },
+      {
+        onSuccess: () => {
+          notification({ title: "복지포인트 확정", message: "복지포인트 확정이 완료되었습니다.", color: "green" });
+
+          queryClient.invalidateQueries({ queryKey: ["welfares"] });
+          setSelectedRows([]);
+          closeCheck();
+        },
+        onError: () => {
+          notification({ title: "복지포인트 확정", message: "복지포인트 확정을 실패하였습니다.", color: "red" });
+        },
+      }
+    );
+  };
+  const form = useForm<FormValues>({
+    initialValues: {
+      userName: "",
+      gradeIdx: null,
+      confirmYN: null,
+    },
+  });
+
+  const selectDateRange = (date: any) => {
+    setValue(date);
+    const sDate = dayjs(date[0]).format("YYYY-MM-DD");
+    const eDate = dayjs(date[1]).format("YYYY-MM-DD");
+    form.setFieldValue("sDate", sDate);
+    form.setFieldValue("eDate", eDate || sDate);
+  };
+
+  const submitSearch = async (values: any) => {
+    const temp = cleanObject(values, "gradeIdx");
+
+    const result = { ...temp, pageNo: 1 };
+
+    setSearchParam(result);
+  };
+
+  const rows = data?.data.data.welfare.map((element: TWelfares, index: number) => (
+    <Table.Tr key={element.welfareIdx} bg={selectedRows.includes(element.welfareIdx) ? "var(--mantine-color-blue-light)" : undefined}>
       <Table.Td>
         <Checkbox
           size="xs"
           radius="sm"
           aria-label="Select row"
-          checked={selectedRows.includes(element.position)}
+          checked={selectedRows.includes(element.welfareIdx)}
           onChange={(event) =>
             setSelectedRows(
-              event.currentTarget.checked ? [...selectedRows, element.position] : selectedRows.filter((position) => position !== element.position)
+              event.currentTarget.checked ? [...selectedRows, element.welfareIdx] : selectedRows.filter((welfareIdx) => welfareIdx !== element.welfareIdx)
             )
           }
         />
       </Table.Td>
-      <Table.Td>{element.position}</Table.Td>
-      <Table.Td>{element.grade}</Table.Td>
-      <Table.Td>{element.name}</Table.Td>
-      <Table.Td>{element.payee}</Table.Td>
+      <Table.Td>{index + 1}</Table.Td>
+      <Table.Td>{element.gradeName}</Table.Td>
+      <Table.Td>{element.userName}</Table.Td>
+      <Table.Td>{element.payerName}</Table.Td>
 
-      <Table.Td>{element.place}</Table.Td>
+      <Table.Td>{element.content}</Table.Td>
 
       <Table.Td>
-        <NumberFormatter thousandSeparator value={12000} suffix=" 원" />
+        <NumberFormatter thousandSeparator value={element.amount || 0} suffix=" 원" />
       </Table.Td>
       <Table.Td>{element.targetDay}</Table.Td>
       <Table.Td>
-        <Checkbox size="xs" label="2024-11-14" />
+        <Checkbox
+          checked={element.confirmYN === "Y" ? true : false}
+          onChange={() => {}}
+          size="sm"
+          label={element.confirmDate ? dayjs(element.confirmDate).format("YYYY-MM-DD") : "미확정"}
+        />
       </Table.Td>
     </Table.Tr>
   ));
+
   return (
     <Flex direction={"column"} h={"100%"} styles={{ root: { overflow: "hidden" } }}>
       <Title order={3} mb={"lg"}>
         복지포인트 내역 조회
       </Title>
       <Group justify="space-between" mb={"md"} align="flex-end">
-        <Group gap={"xs"} align="end">
-          <DatePickerInput
-            valueFormat="MM월 D일 dddd"
-            firstDayOfWeek={0}
-            clearable
-            miw={100}
-            type="range"
-            label="작성일"
-            placeholder="작성일 선택"
-            locale="ko"
-            allowSingleDateInRange
-            value={value}
-            onChange={setValue}
-          />
-          <Input.Wrapper label="성명">
-            <Input placeholder="검색 대상의 성명을 입력해 주세요." radius="md" />
-          </Input.Wrapper>
+        <form onSubmit={form.onSubmit(submitSearch)}>
+          <Group gap={"xs"} align="end">
+            <DatePickerInput
+              valueFormat="MM월 D일 dddd"
+              firstDayOfWeek={0}
+              miw={100}
+              type="range"
+              label="작성일"
+              placeholder="작성일 선택"
+              locale="ko"
+              allowSingleDateInRange
+              value={value}
+              onChange={selectDateRange}
+              defaultValue={[dayjs().startOf("month").toDate(), dayjs().endOf("month").toDate()]}
+            />
+            <Select
+              label={GRADE_NAME_LABEL}
+              data={gradeIdData || []}
+              clearable
+              placeholder="직급 선택"
+              w={100}
+              key={form.key("gradeIdx")}
+              {...form.getInputProps("gradeIdx")}
+            />
+            <Input.Wrapper label="성명">
+              <Input w={240} placeholder="검색 대상의 성명을 입력해 주세요." radius="md" key={form.key("userName")} {...form.getInputProps("userName")} />
+            </Input.Wrapper>
 
-          <Button size="sm" radius={"md"}>
-            검색
-          </Button>
-        </Group>
+            <Button size="sm" radius={"md"} type="submit">
+              검색
+            </Button>
+          </Group>
+        </form>
         <Group>
           <Button variant="light" size="sm" radius={"md"} rightSection={<IconCircleChecked width="15" height="15" />} onClick={openCheck}>
             사용내역 확인
@@ -150,11 +219,11 @@ function page() {
       <Modal opened={check} onClose={closeCheck} centered title="내역 확인">
         <Stack>
           <Alert variant="outline" radius="md" title="해당 내역을 확정 하시겠습니까?" icon={<IconInfo />}>
-            총 5개 내역을 확정합니다.
+            총 {selectedRows.length}개 내역을 확정합니다.
           </Alert>
           <Group wrap="nowrap">
-            <Button variant="light" fullWidth>
-              확인
+            <Button onClick={confirmWelfare} fullWidth>
+              확정하기
             </Button>
             <Button variant="light" color="gray" fullWidth onClick={closeCheck}>
               닫기
