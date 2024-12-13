4aeb03a7ba9fdab03e53704f2734eab950f5c407
aksel26 <kevinxkim2023@gmail.com>
2024-12-13 17:43:54 +0900

    식대 유형 badge

---
+++ src/app/components/template/meal/MealType.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Badge } from "@mantine/core";
+
+export const MealType = (mealType: string | undefined) => {
+  switch (mealType) {
+    case "lunch":
+      return (
+        <Badge variant="light" radius="sm">
+          중식
+        </Badge>
+      );
+    case "breakfase":
+      return (
+        <Badge radius={"sm"} variant="light" color="yellow">
+          조식
+        </Badge>
+      );
+    default:
+      "dinner";
+      return (
+        <Badge radius={"sm"} variant="light" color="violet">
+          석식
+        </Badge>
+      );
+  }
+};
