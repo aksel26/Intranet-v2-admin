d02cf2433f5819e4b3037d8549682aca80e20989
aksel26 <kevinxkim2023@gmail.com>
2025-01-06 18:53:29 +0900

    table header enum

--- src/app/enums/tableHeader.ts
+++ src/app/enums/tableHeader.ts
@@ -5,10 +5,32 @@ export const MEAL_SETTLEMENT_HEADER = ["", "No.", "직급", "성명", "총 금�
 export const MEAL_EXPENSES_HEADER = ["No", "직급", "성명", "유형", "사용처", "사용 금액", "작성일"];
 export const MEAL_CONFIG_HEADER = ["No", "직급", "성명", "총 금액", "근무일 수", "기본금액"];
 
+export const WELFARE_SETTLEMENT_HEADER = ["No.", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산여부", "비고"];
 export const WELFARE_CONFIG_HEADER = ["No.", "직급", "성명", "총 사용가능 금액", "비고"];
+
+export const ACTIVITY_SETTLEMENT_HEADER = ["No.", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산여부", "비고"];
 
 export const QNA_HEADER = ["No.", "직급", "성명", "연락처", "유형", "내용", "답변여부"];
 
 export const NOTICE_HEADER = ["No.", "제목", "작성자", "수정일", "등록일"];
 
-export const VACATION_DETAIL_HEADER = ["회계연도", "사용일자", "분류", "차감갯수", "잔여갯수"];
+export const VACATION_DETAIL_HEADER = ["회계연도", "사용일자", "승인자", "승인여부", "분류", "차감갯수", "잔여갯수", "수정일", "등록일", "승인일", ""];
+
+export const ATTENDANCE_HEADER = [
+  "직원 ID",
+  "직급",
+  "성명",
+  "부서",
+  "출근 날짜",
+  "출근 시간",
+  "퇴근 시간",
+  "근무 시간",
+  "초과 시간",
+  "지각 여부",
+  "근태 상태",
+  "수정 사유",
+  "출근 기기",
+  "특이사항",
+  "수정일",
+  "등록일",
+];
