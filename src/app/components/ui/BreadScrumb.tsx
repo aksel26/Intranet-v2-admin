1bab768f769da98c2053e2934d4e60f4564ac86c
aksel26 <kevinxkim2023@gmail.com>
2025-01-06 18:53:22 +0900

    breadscrumbs 컴포넌트 href

--- src/app/components/ui/BreadScrumb.tsx
+++ src/app/components/ui/BreadScrumb.tsx
@@ -1,21 +1,25 @@
 "use client";
-import { Group, ThemeIcon, Title } from "@mantine/core";
+import { Anchor, Group, ThemeIcon, Title } from "@mantine/core";
 import React from "react";
 import IconArrowRight from "/public/icons/arrow-right.svg";
+import Link from "next/link";
 
 function BreadScrumb({ level }: any) {
   return (
     <Group align="center">
       {level.map((item: any, index: number, arr: any) => (
         <Group align="center" gap={"xs"} key={index}>
-          <Title
+          <Anchor
+            component={Link}
+            href={item.href}
             key={index}
-            order={4}
+            // order={4}
+            fz={"h4"}
             fw={arr.length !== index + 1 ? 500 : 600}
             c={arr.length !== index + 1 ? "dimmed" : "black"}
           >
             {item.title}
-          </Title>
+          </Anchor>
           {arr.length !== index + 1 ? (
             <ThemeIcon color="gray" variant="transparent">
               <IconArrowRight />
--- src/app/enums/breadscrumbs.ts
+++ src/app/enums/breadscrumbs.ts
@@ -3,3 +3,19 @@ export const BREADSCRUMBS_WELFARE_CONFIG = [
   { title: "복지포인트 관리", href: "#" },
   { title: "금액설정 및 정산", href: "#" },
 ];
+
+export const BREADSCRUMBS_VACATION_LIST = [
+  { title: "근태", href: "#" },
+  { title: "휴가 관리", href: "#" },
+];
+export const BREADSCRUMBS_VACATION_DETAIL = [
+  { title: "근태", href: "#" },
+  { title: "휴가 관리", href: "/main/attendance/vacation" },
+  { title: "휴가 상세 조회", href: "#" },
+];
+
+export const BREADSCRUMBS_ATTENDANCE = [
+  { title: "근태", href: "#" },
+  { title: "근태 관리", href: "#" },
+  { title: "출퇴근 관리", href: "#" },
+];
