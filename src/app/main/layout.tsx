86d1683a0ee807f15b7fc75f55af6e44ce81c53e
aksel26 <kevinxkim2023@gmail.com>
2025-01-06 18:53:43 +0900

    navLink 수정

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,24 +1,13 @@
 "use client";
-import {
-  AppShell,
-  Avatar,
-  Burger,
-  Button,
-  Container,
-  Flex,
-  Group,
-  Image,
-  NavLink,
-  Text,
-} from "@mantine/core";
+import { AppShell, Burger, Button, Container, Flex, Group, Image, NavLink, Text } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import dayjs from "dayjs";
 import NextImage from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { UserInfo } from "../components/Global/UserInfo";
 import { HEIGHT } from "../enums/design";
 import myImage from "/public/images/ACG_LOGO_GRAY.png";
-import { UserInfo } from "../components/Global/UserInfo";
-import dayjs from "dayjs";
 
 function layout({
   children,
@@ -64,11 +53,11 @@ function layout({
 
         <UserInfo />
         <NavLink href="#required-for-focus" label="근태" childrenOffset={28}>
-          <NavLink label="근태 관리" />
-          <NavLink
-            label="휴가 관리"
-            onClick={() => movePage("attendance/vacation")}
-          />
+          <NavLink label="근태 관리">
+            <NavLink label="출퇴근 관리" onClick={() => movePage("attendance")} />
+            <NavLink label="초과근무 관리" onClick={() => movePage("overwork")} />
+          </NavLink>
+          <NavLink label="휴가 관리" onClick={() => movePage("attendance/vacation")} />
           <NavLink label="시간외 근무" />
           {/* <NavLink label="활동비 관리" /> */}
         </NavLink>
@@ -76,34 +65,19 @@ function layout({
           <NavLink label="식대 관리">
             <NavLink label="사용내역" onClick={() => movePage("meal")} />
             <NavLink label="통계" onClick={() => movePage("meal/stats")} />
-            <NavLink
-              label="금액설정 및 정산"
-              onClick={() => movePage("meal/settlement")}
-            />
-            <NavLink
-              label="점심조"
-              onClick={() => movePage("meal/lunchGroup")}
-            />
+            <NavLink label="금액설정 및 정산" onClick={() => movePage("meal/settlement")} />
+            <NavLink label="점심조" onClick={() => movePage("meal/lunchGroup")} />
           </NavLink>
           <NavLink label="복지포인트 관리">
             <NavLink label="사용내역" onClick={() => movePage("welfare")} />
             <NavLink label="통계" onClick={() => movePage("welfare/stats")} />
-            <NavLink
-              label="금액설정 및 정산"
-              onClick={() => movePage("welfare/settlement")}
-            />
-            <NavLink
-              label="설정 ⛔️"
-              onClick={() => movePage("welfare/config")}
-            />
+            <NavLink label="금액설정 및 정산" onClick={() => movePage("welfare/settlement")} />
+            <NavLink label="설정 ⛔️" onClick={() => movePage("welfare/config")} />
           </NavLink>
           <NavLink label="활동비 관리">
             <NavLink label="조회" onClick={() => movePage("activity")} />
             <NavLink label="통계" onClick={() => movePage("activity/stats")} />
-            <NavLink
-              label="금액설정 및 정산"
-              onClick={() => movePage("activity/settlement")}
-            />
+            <NavLink label="금액설정 및 정산" onClick={() => movePage("activity/settlement")} />
             <NavLink label="설정" onClick={() => movePage("activity/config")} />
           </NavLink>
         </NavLink>
@@ -123,12 +97,7 @@ function layout({
         </NavLink>
       </AppShell.Navbar>
       <AppShell.Main>
-        <Container
-          h={
-            "calc(100vh - (var(--app-shell-footer-height) + var(--app-shell-header-height))"
-          }
-          fluid
-        >
+        <Container h={"calc(100vh - (var(--app-shell-footer-height) + var(--app-shell-header-height))"} fluid>
           {children}
         </Container>
       </AppShell.Main>
