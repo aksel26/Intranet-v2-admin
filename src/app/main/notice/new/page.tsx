0fd3bcc939fb127c00e3c76294bd25d9a8878e27
aksel26 <kevinxkim2023@gmail.com>
2025-01-12 01:30:15 +0900

    공지사항 작성 (이미지 d&d, text editor)

--- src/app/main/notice/new/page.tsx
+++ src/app/main/notice/new/page.tsx
@@ -1,23 +1,36 @@
 "use client";
 import * as postApi from "@/app/api/post/postApi";
+import TextEditorWrapper from "@/app/components/notice/TextEditorWrapper";
 import BreadScrumb from "@/app/components/ui/BreadScrumb";
 import { BREADSCRUMBS_NOTICE_CREATE } from "@/app/enums/breadscrumbs";
 import useStorageInfo from "@/app/hooks/useStorageInfo";
+import { convertFileUnit } from "@/app/utils/convertFileUnit";
 import notification from "@/app/utils/notification";
-import { Button, FileButton, Flex, Group, Input, ScrollArea, Stack, Text, Textarea, TextInput } from "@mantine/core";
+import {
+  ActionIcon,
+  Anchor,
+  Button,
+  Flex,
+  Group,
+  Modal,
+  ScrollArea,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core";
+import { Dropzone, MIME_TYPES } from "@mantine/dropzone";
 import { useForm } from "@mantine/form";
-import { useMutation } from "@tanstack/react-query";
-import Image from "next/image";
+import { useDisclosure } from "@mantine/hooks";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
-
-interface UploadResponse {
-  url: string;
-  message: string;
-}
+import IconImage from "/public/icons/photo-scan.svg";
+import IconUpload from "/public/icons/upload.svg";
+import IconX from "/public/icons/x.svg";
 
 export default function page() {
   const router = useRouter();
+  const queryClient = useQueryClient();
   const { userInfo } = useStorageInfo();
   const form = useForm({
     mode: "uncontrolled",
@@ -27,6 +40,10 @@ export default function page() {
       imageUrl: "",
     },
   });
+
+  const [openedPreview, { open: openPreviewModal, close: closePreviewModal }] =
+    useDisclosure(false);
+
   const [preview, setPreview] = useState<string | null>(null);
 
   const { mutate } = useMutation({
@@ -35,6 +52,8 @@ export default function page() {
 
   const [file, setFile] = useState<File | null>(null);
   const resetRef = useRef<() => void>(null);
+
+  const [content, onChange] = useState<any>();
 
   // 파일이 변경될 때마다 미리보기 URL 생성
   useEffect(() => {
@@ -50,13 +69,14 @@ export default function page() {
     return () => URL.revokeObjectURL(objectUrl);
   }, [file]);
 
-  const upload = (file: File | null) => {
+  const upload = (file: File[] | null | any) => {
+    console.log("file: ", file);
     if (file) {
-      if (!file.type.startsWith("image/")) {
+      if (!file[0].type.startsWith("image/")) {
         alert("이미지 파일만 업로드 가능합니다.");
         return;
       }
-      setFile(file);
+      setFile(file[0]);
     }
   };
 
@@ -73,77 +93,166 @@ export default function page() {
     mutate(
       {
         title: data.title,
-        content: data.content,
+        content: content,
         noticeImage: file,
       },
       {
-        onSuccess: (res) => {
-          notification({ color: "green", message: "공지사항이 작성되었습니다.", title: "공지사항 작성" });
+        onSuccess: async (res) => {
+          await queryClient.invalidateQueries({ queryKey: ["notices"] });
+          notification({
+            color: "green",
+            message: "공지사항이 작성되었습니다.",
+            title: "공지사항 작성",
+          });
+
           router.back();
         },
         onError: (error) => {
-          notification({ color: "red", message: "공지사항에 문제가 발생하였습니다..", title: "공지사항 작성" });
+          notification({
+            color: "red",
+            message: "공지사항에 문제가 발생하였습니다..",
+            title: "공지사항 작성",
+          });
         },
       }
     );
   };
 
+  const openPreview = () => {
+    openPreviewModal();
+  };
+
   return (
-    <Flex direction={"column"} h={"100%"} styles={{ root: { overflow: "hidden" } }}>
+    <Flex
+      direction={"column"}
+      h={"100%"}
+      styles={{ root: { overflow: "hidden" } }}
+    >
       <BreadScrumb level={BREADSCRUMBS_NOTICE_CREATE} />
 
-      <ScrollArea>
+      <ScrollArea mt={"md"}>
         <form onSubmit={form.onSubmit(submitNotice)}>
           <Stack gap={"md"}>
-            <Input.Wrapper label="작성자" styles={{ label: { fontWeight: 600 } }}>
-              <Input value={userInfo?.adminName || ""} onChange={() => {}} variant="unstyled" />
-            </Input.Wrapper>
+            <TextInput
+              label="작성자"
+              value={userInfo?.adminName || ""}
+              onChange={() => {}}
+              variant="unstyled"
+              readOnly
+            />
 
             <TextInput
-              styles={{ label: { fontSize: "var(--mantine-font-size-sm)", fontWeight: 600 } }}
+              styles={{
+                label: {
+                  fontSize: "var(--mantine-font-size-sm)",
+                  fontWeight: 600,
+                },
+              }}
               key={form.key("title")}
               {...form.getInputProps("title")}
               label="제목"
               placeholder="제목을 입력해 주세요"
+              description="최대 100자까지 입력 가능합니다."
             />
 
-            <Textarea
-              styles={{ label: { fontSize: "var(--mantine-font-size-sm)", fontWeight: 600 } }}
-              label="내용입력"
-              placeholder="내용을 입력해 주세요."
-              autosize
-              minRows={10}
-              key={form.key("content")}
-              {...form.getInputProps("content")}
-            />
-
-            <Stack>
+            <Stack gap={2}>
+              <Text fz={"sm"} fw={600}>
+                내용
+              </Text>
+              <TextEditorWrapper value={content} onChange={onChange} />
+            </Stack>
+            <Stack gap={2}>
               <Text fz={"sm"} fw={600}>
                 첨부파일
               </Text>
-              {preview ? <Image width={200} height={200} alt="preview" src={preview} /> : null}
-              <Group justify="start">
-                <Stack>
-                  {file && (
-                    <Text size="sm" mt="sm">
-                      파일명: {file.name}
-                    </Text>
-                  )}
 
-                  <Group>
-                    <FileButton resetRef={resetRef} onChange={upload} accept="image/png,image/jpeg">
-                      {(props) => (
-                        <Button variant="light" {...props}>
-                          파일 업로드
-                        </Button>
-                      )}
-                    </FileButton>
-                    <Button disabled={!file} color="red" onClick={clearFile}>
-                      업로드 취소
-                    </Button>
-                  </Group>
-                </Stack>
-              </Group>
+              <Dropzone
+                w={"100%"}
+                accept={[
+                  MIME_TYPES.png,
+                  MIME_TYPES.jpeg,
+                  MIME_TYPES.svg,
+                  MIME_TYPES.gif,
+                  MIME_TYPES.webp,
+                ]}
+                onDrop={(files) => upload(files)}
+                onReject={(files) => console.log("rejected files", files)}
+                maxSize={5 * 1024 ** 2}
+                // accept={IMAGE_MIME_TYPE}
+                // {...props}
+              >
+                <Group
+                  justify="center"
+                  gap="xl"
+                  style={{ pointerEvents: "none" }}
+                >
+                  <Dropzone.Accept>
+                    <Group>
+                      <IconUpload width="40" strokeWidth="1.2" height="40" />
+                      <div>
+                        <Text size="md" inline>
+                          첨부할 파일을 여기에 끌어다 놓거나, 파일 선택 버튼을
+                          눌러 파일을 직접 선택해주세요.
+                        </Text>
+                        <Text size="sm" c="dimmed" inline mt={7}>
+                          파일 1개당 크기는 10mb를 초과할 수 없습니다.
+                        </Text>
+                        <Text size="sm" c="dimmed" inline mt={7}>
+                          가능한 파일 형식 : png, jpeg, svg, gif, webp
+                        </Text>
+                      </div>
+                    </Group>
+                  </Dropzone.Accept>
+                  <Dropzone.Reject>
+                    <Group>
+                      <IconX width="40" strokeWidth="1.2" height="40" />
+                      <div>
+                        <Text size="md" inline>
+                          파일 형식 또는 용량을 확인해 주세요.
+                        </Text>
+                        <Text size="sm" c="dimmed" inline mt={7}>
+                          파일 1개당 크기는 10mb를 초과할 수 없습니다.
+                        </Text>
+                        <Text size="sm" c="dimmed" inline mt={7}>
+                          가능한 파일 형식 : png, jpeg, svg, gif, webp
+                        </Text>
+                      </div>
+                    </Group>
+                  </Dropzone.Reject>
+                  <Dropzone.Idle>
+                    <Group>
+                      <IconImage width="40" strokeWidth="1.2" height="40" />
+                      <div>
+                        <Text size="md" inline>
+                          첨부할 파일을 여기에 끌어다 놓거나, 파일 선택 버튼을
+                          눌러 파일을 직접 선택해주세요.
+                        </Text>
+                        <Text size="sm" c="dimmed" inline mt={7}>
+                          파일 1개당 크기는 10mb를 초과할 수 없습니다.
+                        </Text>
+                        <Text size="sm" c="dimmed" inline mt={7}>
+                          가능한 파일 형식 : png, jpeg, svg, gif, webp
+                        </Text>
+                      </div>
+                    </Group>
+                  </Dropzone.Idle>
+                </Group>
+              </Dropzone>
+
+              {file && (
+                <Group align="center">
+                  <Anchor onClick={openPreview} size="sm">
+                    {`${file.name}, [${convertFileUnit(file.size)}]`}
+                  </Anchor>
+                  <ActionIcon
+                    variant="subtle"
+                    aria-label="removeImg"
+                    onClick={clearFile}
+                  >
+                    <IconX style={{ width: "70%", height: "70%" }} />
+                  </ActionIcon>
+                </Group>
+              )}
             </Stack>
 
             <Group justify="center">
@@ -155,6 +264,13 @@ export default function page() {
           </Stack>
         </form>
       </ScrollArea>
+      <Modal
+        opened={openedPreview}
+        onClose={closePreviewModal}
+        title="미리보기"
+      >
+        {preview && <img src={preview} alt="preview" />}
+      </Modal>
     </Flex>
   );
 }
