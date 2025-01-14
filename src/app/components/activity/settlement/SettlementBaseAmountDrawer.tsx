22b69bd5b46caf03eb9dd1ef5a839fb56b391141
aksel26 <kevinxkim2023@gmail.com>
2025-01-14 18:39:24 +0900

    활동비 입력 필드 변경 반영

--- src/app/components/activity/settlement/SettlementBaseAmountDrawer.tsx
+++ src/app/components/activity/settlement/SettlementBaseAmountDrawer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Drawer, Group, NumberInput, Radio, Select, Stack } from "@mantine/core";
+import { Button, Drawer, Group, NumberInput, Radio, Select, Stack, Text } from "@mantine/core";
 import React, { useState } from "react";
 import { useForm } from "@mantine/form";
 import dayjs from "dayjs";
@@ -8,19 +8,24 @@ import { MONTH } from "@/app/enums/month";
 import notification from "@/app/utils/notification";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import * as postApi from "@/app/api/post/postApi";
+
 interface FormValues {
   period: string;
-  welfareBudget: number | null;
+  userIdx: null | number;
+  activityBudget: null | number;
+  activityPeople: null | number;
 }
 
-function WelfareBaseAmountDrawer({ opened, close }: any) {
+function SettlementBaseAmountDrawer({ opened, close }: any) {
   const queryClient = useQueryClient();
 
   const form = useForm<FormValues>({
     mode: "uncontrolled",
     initialValues: {
       period: "H1",
-      welfareBudget: null,
+      userIdx: null,
+      activityBudget: null,
+      activityPeople: null,
     },
   });
 
@@ -40,10 +45,29 @@ function WelfareBaseAmountDrawer({ opened, close }: any) {
     });
   };
 
+  const [calculate, setCalculate] = useState({
+    period: "H1",
+    userIdx: null,
+    activityBudget: null,
+    activityPeople: null,
+  });
+  form.watch("period", (values: any) => {
+    setCalculate((prev) => ({ ...prev, period: values.value }));
+  });
+  form.watch("userIdx", (values: any) => {
+    setCalculate((prev) => ({ ...prev, userIdx: values.value }));
+  });
+  form.watch("activityBudget", (values: any) => {
+    setCalculate((prev) => ({ ...prev, activityBudget: values.value }));
+  });
+  form.watch("activityPeople", (values: any) => {
+    setCalculate((prev) => ({ ...prev, activityPeople: values.value }));
+  });
+
   return (
     <Drawer offset={8} size="md" radius="md" opened={opened} onClose={close} title="활동비 기본금액 설정" position="right">
       <form onSubmit={form.onSubmit(submitWelfareBudget)}>
-        <Stack gap={50} py={"md"}>
+        <Stack gap={"xl"} py={"md"}>
           <Radio.Group
             label="적용 기간 설정"
             description="복지포인트가 설정한 기간에 일괄적으로 적용됩니다."
@@ -58,9 +82,18 @@ function WelfareBaseAmountDrawer({ opened, close }: any) {
             </Group>
           </Radio.Group>
 
+          <Select
+            withAsterisk
+            label="대상자 선택"
+            key={form.key("userIdx")}
+            {...form.getInputProps("userIdx")}
+            placeholder="활동비를 부여할 인원을 선택해 주세요."
+            data={["김현근", "박민수", "김대희", "윤이나"]}
+          />
+
           <NumberInput
             withAsterisk
-            label="활동비 금액"
+            label="구성원 당 금액"
             description="설정될 활동비 기본 금액을 입력해 주세요."
             placeholder="숫자를 입력해 주세요."
             thousandSeparator=","
@@ -69,6 +102,27 @@ function WelfareBaseAmountDrawer({ opened, close }: any) {
             key={form.key("activityBudget")}
             {...form.getInputProps("activityBudget")}
           />
+          <NumberInput
+            withAsterisk
+            label="구성원 수"
+            description="본부 또는 팀 구성원 수를 입력해 주세요."
+            placeholder="숫자로 입력해 주세요."
+            thousandSeparator=","
+            hideControls
+            suffix=" 명"
+            key={form.key("activityPeople")}
+            {...form.getInputProps("activityPeople")}
+          />
+
+          <Stack gap={"xs"}>
+            <Text>
+              <Text component="span">{calculate.userIdx}</Text>님의{" "}
+            </Text>
+            <Text>
+              <Text component="span">{calculate.period}</Text> 예상 배정금액은 <Text component="span">{calculate.activityBudget}</Text>원 입니다.{" "}
+            </Text>
+          </Stack>
+
           <Group wrap="nowrap">
             <Button fullWidth type="submit" radius={"md"}>
               저장
@@ -83,4 +137,4 @@ function WelfareBaseAmountDrawer({ opened, close }: any) {
   );
 }
 
-export default WelfareBaseAmountDrawer;
+export default SettlementBaseAmountDrawer;
