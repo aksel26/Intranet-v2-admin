3b730676b9494d8ca7bdefe48da7080428a24564
aksel26 <kevinxkim2023@gmail.com>
2025-01-17 18:10:06 +0900

    dateFormat util

---
+++ src/app/utils/dateFormat.ts
@@ -0,0 +1,10 @@
+import dayjs from "dayjs";
+
+export const dateFormatYYYYMMDD = (date: string | null) => {
+  return dayjs(date).format("YYYY-MM-DD");
+};
+
+export const dateFormatTime = (date: string | null) => {
+  if (!date) return "-";
+  else return dayjs(date).format("HH:mm:ss");
+};
