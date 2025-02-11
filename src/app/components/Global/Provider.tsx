7f8a9d64449ed5577e2fbc3a1365b047e583c79c
aksel26 <kevinxkim2023@gmail.com>
2025-02-11 18:53:29 +0900

    button global theme

--- src/app/components/Global/Provider.tsx
+++ src/app/components/Global/Provider.tsx
@@ -48,6 +48,15 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         "Pretendard Variable, Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, Helvetica Neue, Segoe UI, Apple SD Gothic Neo, Noto Sans KR, Malgun Gothic, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, sans-serif",
     },
     cursorType: "pointer",
+    components: {
+      Button: {
+        styles: {
+          label: {
+            fontWeight: 500,
+          },
+        },
+      },
+    },
   });
   return (
     <QueryClientProvider client={queryClient}>
