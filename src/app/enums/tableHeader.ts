7857a90478a8736810f6505e222c2eff0213ab5f
aksel26 <kevinxkim2023@gmail.com>
2025-02-19 16:01:02 +0900

    출퇴근관리 열 추가 (첨부파일 열)

--- src/app/enums/tableHeader.ts
+++ src/app/enums/tableHeader.ts
@@ -1,5 +1,5 @@
 export const STAFF_TABLE_HEADER = ["No", "소속", "팀명", "직급", "성명", "ID", "계정등급", "연락처", "이메일", "성별", "생년월일", "입사일", "특이사항", ""];
-export const VACATION_TABLE_HEADER = ["", "No", "소속", "팀명", "직급", "성명", "이메일", "총 연차일", "잔여 연차일", "최근 사용일", "특이사항"];
+export const VACATION_TABLE_HEADER = ["", "No", "소속", "팀명", "직급", "성명", "이메일", "총 연차일", "잔여 연차일", "최근 사용일"];
 
 export const MEAL_SETTLEMENT_HEADER = ["", "No.", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산금", "정산여부", "비고"];
 export const MEAL_EXPENSES_HEADER = ["No", "직급", "성명", "유형", "사용처", "사용 금액", "작성일"];
@@ -15,7 +15,7 @@ export const QNA_HEADER = ["No.", "직급", "성명", "연락처", "유형", "�
 
 export const NOTICE_HEADER = ["No.", "제목", "작성자", "등록일"];
 
-export const VACATION_DETAIL_HEADER = ["회계연도", "사용일자", "승인자", "승인여부", "분류", "차감갯수", "잔여갯수", "수정일", "등록일", "승인일", ""];
+export const VACATION_DETAIL_HEADER = ["회계연도", "사용일자", "승인자", "승인여부", "분류", "내용", "차감갯수", "잔여갯수", "수정일", "등록일", "승인일", ""];
 
 export const ATTENDANCE_HEADER = [
   "",
@@ -36,4 +36,5 @@ export const ATTENDANCE_HEADER = [
   "특이사항",
   "수정일",
   "등록일",
+  "첨부파일",
 ];
