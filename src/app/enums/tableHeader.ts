8684a22d6683efd79777de25118b5ef0127b745c
aksel26 <kevinxkim2023@gmail.com>
2025-04-14 15:57:53 +0900

    테이블명 수정

--- src/app/enums/tableHeader.ts
+++ src/app/enums/tableHeader.ts
@@ -5,7 +5,7 @@ export const MEAL_SETTLEMENT_HEADER = ["", "No.", "직급", "성명", "총 금�
 export const MEAL_EXPENSES_HEADER = ["No", "팀명", "직급", "성명", "근태", "유형", "사용처", "사용 금액", "작성일"];
 export const MEAL_CONFIG_HEADER = ["No", "직급", "성명", "총 금액", "근무일 수", "기본금액"];
 
-export const WELFARE_SETTLEMENT_HEADER = ["", "No.", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산여부", "비고"];
+export const WELFARE_SETTLEMENT_HEADER = ["", "팀명", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산여부", "비고"];
 export const WELFARE_CONFIG_HEADER = ["No.", "직급", "성명", "총 사용가능 금액", "비고"];
 
 export const ACTIVITY_HEADER = ["No.", "직급", "성명", "결제자", "사용처", "사용 금액", "작성일", "확정여부"];
