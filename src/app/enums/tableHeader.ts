1ce69baf16dc55debb29be69b65106d325777ae8
aksel26 <kevinxkim2023@gmail.com>
2025-04-15 14:35:44 +0900

    식대 정산 테이블 헤더 수정

--- src/app/enums/tableHeader.ts
+++ src/app/enums/tableHeader.ts
@@ -1,7 +1,7 @@
 export const STAFF_TABLE_HEADER = ["No", "소속", "팀명", "직급", "성명", "ID", "계정등급", "연락처", "이메일", "성별", "생년월일", "입사일", "특이사항", ""];
 export const VACATION_TABLE_HEADER = ["", "직원ID", "소속", "팀", "직급", "성명", "총 연차일", "잔여 연차일", "비고"];
 
-export const MEAL_SETTLEMENT_HEADER = ["", "No.", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산금", "정산여부", "비고"];
+export const MEAL_SETTLEMENT_HEADER = ["", "팀명", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산금", "정산여부", "비고"];
 export const MEAL_EXPENSES_HEADER = ["No", "팀명", "직급", "성명", "근태", "유형", "사용처", "사용 금액", "작성일"];
 export const MEAL_CONFIG_HEADER = ["No", "직급", "성명", "총 금액", "근무일 수", "기본금액"];
 
