0eab423315e049f4df370f89172809061fc75a41
aksel26 <kevinxkim2023@gmail.com>
2025-04-18 18:40:56 +0900

    nav scroll

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { AppShell, Burger, Button, Container, Flex, Group, Image, NavLink, Text } from "@mantine/core";
+import { AppShell, Burger, Button, Container, Flex, Group, Image, NavLink, ScrollArea, Text } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import dayjs from "dayjs";
 import NextImage from "next/image";
@@ -52,46 +52,48 @@ function layout({
         </Group>
 
         <UserInfo />
-        <NavLink href="#required-for-focus" label="근태" childrenOffset={28}>
-          <NavLink label="출퇴근 관리" onClick={() => movePage("attendance")} />
-          <NavLink label="휴가/연차 관리" onClick={() => movePage("attendance/vacation")} />
-          {/* <NavLink label="시간외 근무" onClick={() => movePage("attendance/overwork")} /> */}
-          {/* <NavLink label="활동비 관리" /> */}
-        </NavLink>
-        <NavLink label="비용" childrenOffset={28}>
-          <NavLink label="식대 관리">
-            <NavLink label="사용내역" onClick={() => movePage("meal")} />
-            {/* <NavLink label="통계" onClick={() => movePage("meal/stats")} /> */}
-            <NavLink label="금액설정 및 정산" onClick={() => movePage("meal/settlement")} />
-            {/* <NavLink label="점심조" onClick={() => movePage("meal/lunchGroup")} /> */}
+        <ScrollArea>
+          <NavLink href="#required-for-focus" label="근태" childrenOffset={28}>
+            <NavLink label="출퇴근 관리" onClick={() => movePage("attendance")} />
+            <NavLink label="휴가/연차 관리" onClick={() => movePage("attendance/vacation")} />
+            {/* <NavLink label="시간외 근무" onClick={() => movePage("attendance/overwork")} /> */}
+            {/* <NavLink label="활동비 관리" /> */}
           </NavLink>
-          <NavLink label="복지포인트 관리">
-            <NavLink label="사용내역" onClick={() => movePage("welfare")} />
-            {/* <NavLink label="통계" onClick={() => movePage("welfare/stats")} /> */}
-            <NavLink label="금액설정 및 정산" onClick={() => movePage("welfare/settlement")} />
-            {/* <NavLink label="설정 ⛔️" onClick={() => movePage("welfare/config")} /> */}
+          <NavLink label="비용" childrenOffset={28}>
+            <NavLink label="식대 관리">
+              <NavLink label="사용내역" onClick={() => movePage("meal")} />
+              {/* <NavLink label="통계" onClick={() => movePage("meal/stats")} /> */}
+              <NavLink label="금액설정 및 정산" onClick={() => movePage("meal/settlement")} />
+              {/* <NavLink label="점심조" onClick={() => movePage("meal/lunchGroup")} /> */}
+            </NavLink>
+            <NavLink label="복지포인트 관리">
+              <NavLink label="사용내역" onClick={() => movePage("welfare")} />
+              {/* <NavLink label="통계" onClick={() => movePage("welfare/stats")} /> */}
+              <NavLink label="금액설정 및 정산" onClick={() => movePage("welfare/settlement")} />
+              {/* <NavLink label="설정 ⛔️" onClick={() => movePage("welfare/config")} /> */}
+            </NavLink>
+            <NavLink label="활동비 관리">
+              <NavLink label="사용내역" onClick={() => movePage("activity")} />
+              {/* <NavLink label="통계" onClick={() => movePage("activity/stats")} /> */}
+              <NavLink label="금액설정 및 정산" onClick={() => movePage("activity/settlement")} />
+              {/* <NavLink label="설정" onClick={() => movePage("activity/config")} /> */}
+            </NavLink>
           </NavLink>
-          <NavLink label="활동비 관리">
-            <NavLink label="사용내역" onClick={() => movePage("activity")} />
-            {/* <NavLink label="통계" onClick={() => movePage("activity/stats")} /> */}
-            <NavLink label="금액설정 및 정산" onClick={() => movePage("activity/settlement")} />
-            {/* <NavLink label="설정" onClick={() => movePage("activity/config")} /> */}
+          <NavLink label="설정" childrenOffset={28}>
+            <NavLink label="직원정보 관리" onClick={() => movePage("staff")} />
+            {/* <NavLink label="조직 관리" onClick={() => movePage("org")} /> */}
+
+            {/* <NavLink label="활동비 관리" /> */}
           </NavLink>
-        </NavLink>
-        <NavLink label="설정" childrenOffset={28}>
-          <NavLink label="직원정보 관리" onClick={() => movePage("staff")} />
-          {/* <NavLink label="조직 관리" onClick={() => movePage("org")} /> */}
 
-          {/* <NavLink label="활동비 관리" /> */}
-        </NavLink>
+          <NavLink label="기타" childrenOffset={28}>
+            {/* <NavLink label="문의내역" onClick={() => movePage("qna")} /> */}
+            <NavLink label="공지사항" onClick={() => movePage("notice")} />
+            {/* <NavLink label="조직 관리" onClick={() => movePage("organization")} /> */}
 
-        <NavLink label="기타" childrenOffset={28}>
-          {/* <NavLink label="문의내역" onClick={() => movePage("qna")} /> */}
-          <NavLink label="공지사항" onClick={() => movePage("notice")} />
-          {/* <NavLink label="조직 관리" onClick={() => movePage("organization")} /> */}
-
-          {/* <NavLink label="활동비 관리" /> */}
-        </NavLink>
+            {/* <NavLink label="활동비 관리" /> */}
+          </NavLink>
+        </ScrollArea>
       </AppShell.Navbar>
       <AppShell.Main>
         <Container h={"calc(100vh - (var(--app-shell-footer-height) + var(--app-shell-header-height))"} fluid>
