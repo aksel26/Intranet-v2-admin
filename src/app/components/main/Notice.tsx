ec859030d34020342e96785257960fda53cf6fc4
aksel26 <kevinxkim2023@gmail.com>
2025-04-19 13:35:23 +0900

    최적화

--- src/app/components/main/Notice.tsx
+++ src/app/components/main/Notice.tsx
@@ -6,10 +6,11 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import styles from "@/app/styles/list.module.css";
 import dayjs from "dayjs";
+import { IconChevronRight } from "@tabler/icons-react";
 const Notice = () => {
   const { data, isLoading, isError } = useQuery({
     queryKey: ["notices"],
-    queryFn: () => api.getNotices(),
+    queryFn: () => api.getNotices({ pageNo: 1, perPage: 5 }),
   });
 
   const notices = data?.data.data.notices.slice(0, 4);
@@ -18,48 +19,66 @@ const Notice = () => {
   const movePage = () => router.push("/main/notice");
   const moveDetail = (noticeIdx: number) => router.push(`/main/notice/${noticeIdx}`);
 
+  const LoadingView = () => (
+    <Group justify="center" py={"lg"}>
+      <Loader size={"sm"} />
+    </Group>
+  );
+
+  const EmptyView = () => (
+    <Text ta={"center"} c={"dimmed"} fz={"xs"} py={"lg"}>
+      공지사항 등록 내역이 없어요.
+    </Text>
+  );
+
+  const NoticeList = () => (
+    <List spacing={0} size="sm" center>
+      {notices?.map((record: TNotice, index: number) => (
+        <NoticeItem key={index} record={record} />
+      ))}
+    </List>
+  );
+
+  const NoticeItem = ({ record }: { record: TNotice }) => (
+    <ListItem
+      w={"100%"}
+      onClick={() => moveDetail(record.noticeIdx)}
+      key={record.noticeIdx}
+      className={styles.element}
+      py={"xs"}
+      px={"lg"}
+      styles={{ itemWrapper: { width: "100%" }, itemLabel: { width: "100%" } }}
+    >
+      <Group align="center" justify="space-between" wrap="nowrap">
+        <Text fz={"xs"}>{record.title}</Text>
+        <Group>
+          <Text c={"dimmed"} fz={"xs"}>
+            {record.creatorName}
+          </Text>
+          <Text c={"dimmed"} fz={"xs"}>
+            {`${dayjs(record.createdAt).format("YYYY-MM-DD (dd)")}`}
+          </Text>
+        </Group>
+      </Group>
+    </ListItem>
+  );
+
+  const renderContent = () => {
+    if (isLoading) return <LoadingView />;
+    if (notices?.length === 0) return <EmptyView />;
+    return <NoticeList />;
+  };
+
   return (
-    <Paper shadow="lg" p="lg" radius={"lg"}>
-      <Group justify="space-between">
+    <Paper shadow="lg" py="lg" radius={"lg"}>
+      <Group justify="space-between" mb={"xs"} px={"lg"}>
         <Text fw={600}>공지사항</Text>
-        <Button variant="subtle" size="compact-sm" onClick={() => movePage()}>
+        <Button variant="subtle" size="compact-xs" onClick={() => movePage()} rightSection={<IconChevronRight size={14} />}>
           자세히 보기
         </Button>
       </Group>
 
-      {isLoading ? (
-        <Group justify="center" py={"lg"}>
-          <Loader size={"sm"} />
-        </Group>
-      ) : (
-        <List spacing={0} size="sm" center>
-          {notices?.map((record: TNotice) => {
-            return (
-              <ListItem
-                w={"100%"}
-                onClick={() => moveDetail(record.noticeIdx)}
-                key={record.noticeIdx}
-                className={styles.element}
-                px={"sm"}
-                py={"xs"}
-                styles={{ itemWrapper: { width: "100%" }, itemLabel: { width: "100%" } }}
-              >
-                <Group align="center" justify="space-between" wrap="nowrap">
-                  <Text fz={"sm"}>{record.title}</Text>
-                  <Group>
-                    <Text c={"dimmed"} fz={"sm"}>
-                      {record.creatorName}
-                    </Text>
-                    <Text c={"dimmed"} fz={"sm"}>
-                      {`${dayjs(record.createdAt).format("YYYY-MM-DD (dd)")}`}
-                    </Text>
-                  </Group>
-                </Group>
-              </ListItem>
-            );
-          })}
-        </List>
-      )}
+      {renderContent()}
     </Paper>
   );
 };
