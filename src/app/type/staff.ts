7d42e3710c4ed6ff4170baa63e3a9413679d77ef
aksel26 <kevinxkim2023@gmail.com>
2025-05-28 18:10:59 +0900

    직원조회 type

--- src/app/type/staff.ts
+++ src/app/type/staff.ts
@@ -1,19 +1,34 @@
-export interface TStaffs {
-  userIdx: number;
+export interface User {
   id: string;
+  userIdx: number;
   userName: string;
   userGender: string;
   userCell: string;
-  userAddress: string | null;
+  userAddress: string;
   userEmail: string;
   userBirth: string;
   joinDate: string;
-  hqName: string | null;
+  hqIdx: number;
+  teamIdx: number;
+  gradeIdx: number;
+  hqName: string;
   teamName: string;
   gradeName: string;
   adminRole: string;
-  comment: string | null;
-  userAvail: null;
   adminGradeIdx: number | null;
-  status: string;
+  comment: string;
+  userPersonalEmail: string | null;
+  accountNumber: string | null;
+  accountBank: string | null;
+  passportName: string | null;
+  passportBirth: string | null;
+  passportNo: string | null;
+  passportExpiry: string | null;
+  probationPeriod: string | null;
+  userAvail: string;
+}
+
+export interface UserOption {
+  label: string;
+  value: string;
 }
