1d1ef4cddf2b54c01c6f6f07861a467ecf97c856
aksel26 <kevinxkim2023@gmail.com>
2025-05-28 14:03:44 +0900

    main 레이아웃 css

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Grid, Paper, Text, Title } from "@mantine/core";
+import { Grid, Paper, Stack, Text, Title } from "@mantine/core";
 import dayjs from "dayjs";
 import Attendance from "../components/main/Attendance";
 import MonthlyDrink from "../components/main/MonthlyDrink";
@@ -8,11 +8,8 @@ import Notice from "../components/main/Notice";
 import LunchGroup from "../components/meal/config";
 function page() {
   return (
-    <Grid>
-      {/* <Grid.Col>
-        <SearchBar />
-      </Grid.Col> */}
-      <Grid.Col span={12} ml={5}>
+    <>
+      <Stack mb="sm" gap={"xs"}>
         <Title order={5}>안녕하세요 👋</Title>
         <Title c={"gray.5"} order={5}>
           오늘은{" "}
@@ -21,31 +18,33 @@ function page() {
           </Text>{" "}
           입니다.
         </Title>
-      </Grid.Col>
-      <Grid.Col span={1.5}>
-        <Paper shadow="lg" p="lg" radius={"lg"}>
-          <Text fw={500}>오늘의 출근율</Text>
-          <Text fz={"xl"} fw={700}>
-            80%
-          </Text>
-          <Text fz="sm" c={"dimmed"}>
-            30명 / 40명
-          </Text>
-        </Paper>
-      </Grid.Col>
-      <Grid.Col span={4.5}>
-        <Attendance />
-      </Grid.Col>
-      <Grid.Col span={3}>
-        <LunchGroup />
-      </Grid.Col>
-      <Grid.Col span={3}>
-        <MonthlyDrink />
-      </Grid.Col>
-      <Grid.Col span={6}>
-        <Notice />
-      </Grid.Col>
-    </Grid>
+      </Stack>
+      <Grid>
+        {/* <Grid.Col span={1.5}>
+          <Paper shadow="lg" p="lg" radius={"lg"}>
+            <Text fw={500}>오늘의 출근율</Text>
+            <Text fz={"xl"} fw={700}>
+              80%
+            </Text>
+            <Text fz="sm" c={"dimmed"}>
+              30명 / 40명
+            </Text>
+          </Paper>
+        </Grid.Col> */}
+        <Grid.Col span={6}>
+          <Attendance />
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Notice />
+        </Grid.Col>
+        <Grid.Col span={3}>
+          <LunchGroup />
+        </Grid.Col>
+        <Grid.Col span={3}>
+          <MonthlyDrink />
+        </Grid.Col>
+      </Grid>
+    </>
   );
 }
 
