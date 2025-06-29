78f7c1aeecdc3a8c1d3d1630520c69c0699315de
aksel26 <kevinxkim2023@gmail.com>
2025-06-29 18:54:10 +0900

    휴가/연차정보 (개인) 테이블 정보 수정

--- src/app/components/table/vacationDetail/index.tsx
+++ src/app/components/table/vacationDetail/index.tsx
@@ -1,44 +1,62 @@
 import { TVacationDetail } from "@/app/type/vacationDetail";
 import { dateFormatYYYYMMDD } from "@/app/utils/dateFormat";
-import { Button, Group, Table, Text } from "@mantine/core";
+import { Badge, Button, Group, Popover, Table, Text } from "@mantine/core";
+import { IconDots } from "@tabler/icons-react";
 import { memo } from "react";
 
 const ApprovalStatus = ({ element }: { element: any }) => {
   const { confirmYN, confirmDate, rejectDate } = element;
   if (confirmYN === "Y") {
     return (
-      <Text fz={"xs"} c={"green.5"} miw={60}>
+      <Badge variant="light" size="xs" color="lime" radius={"sm"} miw={60}>
         승인완료
-      </Text>
+      </Badge>
     );
   } else if (confirmYN === "R") {
     return (
-      <Text fz={"xs"} c={"red.4"} miw={60}>
+      <Badge variant="light" size="xs" color="red" radius={"sm"} miw={60}>
         반려
         <Text component="span" fz={"xs"} c={"dimmed"} ml={5}>
           ({rejectDate})
         </Text>
-      </Text>
+      </Badge>
     );
-  } else if (confirmYN === "N") {
+  } else if (!confirmYN || confirmYN === "N") {
     return (
-      <Text fz={"xs"} c={"yellow.5"} miw={60}>
+      <Badge variant="light" size="xs" color="blue" radius={"sm"} miw={60}>
         승인 대기
-      </Text>
+      </Badge>
     );
   }
 };
 
-const NoteInput = ({ note, modifyNote, element }: { note: any; modifyNote: any; element: any }) => {
+const NoteInput = ({
+  note,
+  modifyNote,
+  element,
+}: {
+  note: any;
+  modifyNote: any;
+  element: any;
+}) => {
   if (note) {
     return (
-      <Text fz={"xs"} td="underline" onClick={() => modifyNote(element)} styles={{ root: { cursor: "pointer" } }}>
+      <Text
+        fz={"xs"}
+        td="underline"
+        onClick={() => modifyNote(element)}
+        styles={{ root: { cursor: "pointer" } }}
+      >
         {note}
       </Text>
     );
   } else {
     return (
-      <Button size="compact-xs" variant="light" onClick={() => modifyNote(element)}>
+      <Button
+        size="compact-xs"
+        variant="light"
+        onClick={() => modifyNote(element)}
+      >
         등록
       </Button>
     );
@@ -46,59 +64,116 @@ const NoteInput = ({ note, modifyNote, element }: { note: any; modifyNote: any; 
 };
 
 const ApprovalList = ({ element }: { element: any }) => {
-  const { confirmYN } = element;
-  if (confirmYN === "Y") {
-    return <Text fz={"xs"}>{element.confirmPersonName}</Text>;
-  } else {
+  const { approverInfo } = element;
+  console.log("approverInfo: ", approverInfo);
+  if (approverInfo.length === 0) return <Text fz={"xs"}> -</Text>;
+  if (approverInfo.length === 1) {
+    return <Text fz={"xs"}>{approverInfo[0]?.approverName}</Text>;
+  } else if (approverInfo.length >= 2) {
     return (
-      <Group fz={"xs"} c={"dimmed"}>
-        {element.approverInfo.map((person: any) => (
-          <Text key={person.approverName} component="span" fz={"xs"} c={"dimmed"}>
-            {person.approverName}
-          </Text>
-        ))}
-      </Group>
+      <Popover width={200} position="bottom" withArrow shadow="md">
+        <Popover.Target>
+          <Group gap={"xs"} align="center" style={{ cursor: "pointer" }}>
+            <Text size="xs">
+              {approverInfo[0]?.approverName} 외{approverInfo.length - 1}인
+            </Text>
+            <IconDots color="var(--mantine-color-blue-5)" size={15} />
+          </Group>
+        </Popover.Target>
+        <Popover.Dropdown>
+          {approverInfo?.map((approver: any, index: number) => (
+            <Text key={index} fz={"xs"}>
+              {approver.approverName}
+            </Text>
+          ))}
+        </Popover.Dropdown>
+      </Popover>
+    );
+  }
+};
+const CcList = ({ element }: { element: any }) => {
+  const { ccUserInfo } = element;
+  if (ccUserInfo.length === 0) return <Text fz={"xs"}> -</Text>;
+  if (ccUserInfo.length === 1) {
+    return <Text fz={"xs"}>{ccUserInfo[0]?.ccUserName}</Text>;
+  } else if (ccUserInfo.length >= 2) {
+    return (
+      <Popover width={200} position="bottom" withArrow shadow="md">
+        <Popover.Target>
+          <Group gap={"xs"} align="center" style={{ cursor: "pointer" }}>
+            <Text size="xs">
+              {ccUserInfo[0]?.ccUserName} 외{ccUserInfo.length - 1}인
+            </Text>
+            <IconDots color="var(--mantine-color-blue-5)" size={15} />
+          </Group>
+        </Popover.Target>
+        <Popover.Dropdown>
+          {ccUserInfo?.map((cc: any, index: number) => (
+            <Text key={index} fz={"xs"}>
+              {cc.ccUserName}
+            </Text>
+          ))}
+        </Popover.Dropdown>
+      </Popover>
     );
   }
 };
 
-export const VacationDetil = memo(({ data, deleteDetail, modifyNote, selectAttachment }: any) => {
-  return data?.map((element: TVacationDetail, index: number) => (
-    <Table.Tr key={index} fz={"xs"}>
-      <Table.Td>{element.commuteDate}</Table.Td>
-      <Table.Td>{element.leaveType}</Table.Td>
-      <Table.Td>
-        <ApprovalStatus element={element} />
-      </Table.Td>
-      <Table.Td>
-        <Text fz={"xs"}>{element.confirmDate || "-"}</Text>
-      </Table.Td>
-      <Table.Td>
-        <ApprovalList element={element} />
-      </Table.Td>
-      <Table.Td>{element.annualLeaveReduceUnit}</Table.Td>
-      <Table.Td>{element.remainingAnnualLeaveQuota}</Table.Td>
-      <Table.Td>
-        <NoteInput note={element.note} modifyNote={modifyNote} element={element} />
-      </Table.Td>
-      <Table.Td>
-        {element.imageUrl ? (
-          <Button onClick={() => selectAttachment(element)} size="compact-xs" variant="light">
-            조회
+export const VacationDetil = memo(
+  ({ data, deleteDetail, modifyNote, selectAttachment }: any) => {
+    return data?.map((element: TVacationDetail, index: number) => (
+      <Table.Tr key={index} fz={"xs"}>
+        <Table.Td>{element.commuteDate}</Table.Td>
+        <Table.Td>{element.leaveType}</Table.Td>
+        <Table.Td>
+          <ApprovalStatus element={element} />
+        </Table.Td>
+        <Table.Td>
+          <Text fz={"xs"}>{element.confirmDate || "-"}</Text>
+        </Table.Td>
+        <Table.Td>
+          <ApprovalList element={element} />
+        </Table.Td>
+        <Table.Td>
+          <CcList element={element} />
+        </Table.Td>
+        <Table.Td>{element.annualLeaveReduceUnit}</Table.Td>
+        <Table.Td>{element.remainingAnnualLeaveQuota}</Table.Td>
+        <Table.Td>
+          <NoteInput
+            note={element.note}
+            modifyNote={modifyNote}
+            element={element}
+          />
+        </Table.Td>
+        <Table.Td>
+          {element.imageUrl ? (
+            <Button
+              onClick={() => selectAttachment(element)}
+              size="compact-xs"
+              variant="light"
+            >
+              조회
+            </Button>
+          ) : (
+            <Text c={"dimmed"} fz={"xs"}>
+              없음
+            </Text>
+          )}
+        </Table.Td>
+        {/* <Table.Td>{dateFormatYYYYMMDD(element.updatedAt)}</Table.Td>
+      <Table.Td>{dateFormatYYYYMMDD(element.createdAt)}</Table.Td> */}
+        <Table.Td>
+          <Button
+            color="red"
+            variant="light"
+            size="compact-xs"
+            onClick={() => deleteDetail(element)}
+          >
+            삭제
           </Button>
-        ) : (
-          <Text c={"dimmed"} fz={"xs"}>
-            없음
-          </Text>
-        )}
-      </Table.Td>
-      <Table.Td>{dateFormatYYYYMMDD(element.updatedAt)}</Table.Td>
-      <Table.Td>{dateFormatYYYYMMDD(element.createdAt)}</Table.Td>
-      <Table.Td>
-        <Button color="red" variant="light" size="compact-xs" onClick={() => deleteDetail(element)}>
-          삭제
-        </Button>
-      </Table.Td>
-    </Table.Tr>
-  ));
-});
+        </Table.Td>
+      </Table.Tr>
+    ));
+  }
+);
--- src/app/enums/tableHeader.ts
+++ src/app/enums/tableHeader.ts
@@ -15,20 +15,116 @@ export const STAFF_TABLE_HEADER = [
   "재직상태",
   "",
 ];
-export const VACATION_TABLE_HEADER = ["", "직원ID", "소속", "팀", "직급", "성명", "총 연차일", "잔여 연차일", "최근 사용일", "비고"];
+export const VACATION_TABLE_HEADER = [
+  "",
+  "직원ID",
+  "소속",
+  "팀",
+  "직급",
+  "성명",
+  "총 연차일",
+  "잔여 연차일",
+  "최근 사용일",
+  "비고",
+];
 
-export const MEAL_SETTLEMENT_HEADER = ["", "팀명", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산금", "정산여부", "비고"];
-export const MEAL_EXPENSES_HEADER = ["No", "팀명", "직급", "성명", "근태", "유형", "사용처", "사용 금액", "비고", "작성일"];
-export const MEAL_CONFIG_HEADER = ["No", "직급", "성명", "총 금액", "근무일 수", "기본금액"];
+export const MEAL_SETTLEMENT_HEADER = [
+  "",
+  "팀명",
+  "직급",
+  "성명",
+  "총 금액",
+  "사용 금액",
+  "잔액",
+  "정산금",
+  "정산여부",
+  "비고",
+];
+export const MEAL_EXPENSES_HEADER = [
+  "No",
+  "팀명",
+  "직급",
+  "성명",
+  "근태",
+  "유형",
+  "사용처",
+  "사용 금액",
+  "비고",
+  "작성일",
+];
+export const MEAL_CONFIG_HEADER = [
+  "No",
+  "직급",
+  "성명",
+  "총 금액",
+  "근무일 수",
+  "기본금액",
+];
 
-export const WELFARES_HEADER = ["", "팀명", "직급", "성명", "사용처", "금액", "결제자", "작성일", "비고", "관리자 승인 상태"];
-export const WELFARE_SETTLEMENT_HEADER = ["", "팀명", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산여부", "비고"];
-export const WELFARE_CONFIG_HEADER = ["No.", "직급", "성명", "총 사용가능 금액", "비고"];
+export const WELFARES_HEADER = [
+  "",
+  "팀명",
+  "직급",
+  "성명",
+  "사용처",
+  "금액",
+  "결제자",
+  "작성일",
+  "비고",
+  "관리자 승인 상태",
+];
+export const WELFARE_SETTLEMENT_HEADER = [
+  "",
+  "팀명",
+  "직급",
+  "성명",
+  "총 금액",
+  "사용 금액",
+  "잔액",
+  "정산여부",
+  "비고",
+];
+export const WELFARE_CONFIG_HEADER = [
+  "No.",
+  "직급",
+  "성명",
+  "총 사용가능 금액",
+  "비고",
+];
 
-export const ACTIVITY_HEADER = ["", "팀명", "직급", "성명", "사용처", "사용 금액", "결제자", "작성일", "관리자 승인 여부"];
-export const ACTIVITY_SETTLEMENT_HEADER = ["", "팀명", "직급", "성명", "총 금액", "사용 금액", "잔액", "정산금", "정산여부", "비고"];
+export const ACTIVITY_HEADER = [
+  "",
+  "팀명",
+  "직급",
+  "성명",
+  "사용처",
+  "사용 금액",
+  "결제자",
+  "작성일",
+  "관리자 승인 여부",
+];
+export const ACTIVITY_SETTLEMENT_HEADER = [
+  "",
+  "팀명",
+  "직급",
+  "성명",
+  "총 금액",
+  "사용 금액",
+  "잔액",
+  "정산금",
+  "정산여부",
+  "비고",
+];
 
-export const QNA_HEADER = ["No.", "직급", "성명", "연락처", "유형", "내용", "답변여부"];
+export const QNA_HEADER = [
+  "No.",
+  "직급",
+  "성명",
+  "연락처",
+  "유형",
+  "내용",
+  "답변여부",
+];
 
 export const NOTICE_HEADER = ["No.", "제목", "작성자", "등록일"];
 
@@ -38,12 +134,13 @@ export const VACATION_DETAIL_HEADER = [
   "결재상태",
   "결재일자",
   "결재자",
+  "참조",
   "차감 개수",
   "잔여 개수",
   "내용",
   "첨부파일",
-  "수정일시",
-  "등록일시",
+  // "수정일시",
+  // "등록일시",
   "",
 ];
 
